d5964b16917f9efad2d7619eb9a4986ba3118ec4
Khalil-Abboud <khalil.ad.1998.98.com>
2026-05-07 13:48:55 +0300

    Add task creation from React frontend

--- react-frontend/src/TasksPage.jsx
+++ react-frontend/src/TasksPage.jsx
@@ -6,6 +6,11 @@ function TasksPage({ onLogout }) {
     const [tasks, setTasks] = useState([])
     const [nextPage, setNextPage] = useState(null)
     const [previousPage, setPreviousPage] = useState(null)
+    const [title, setTitle] = useState('')
+    const [description, setDescription] = useState('')
+    const [priority, setPriotiy] = useState('medium')
+    const [dueDate, setDueDate] = useState('')
+
     async function loadTasks(url = `${API_BASE_URL}/tasks`) {
         const token = localStorage.getItem('token')
         const response = await fetch(url, {
@@ -14,7 +19,6 @@ function TasksPage({ onLogout }) {
             },
         })
         const data = await response.json()
-        console.log('Tasks', data)
         setTasks(data.results)
         setNextPage(data.next)
         setPreviousPage(data.previous)
@@ -24,10 +28,68 @@ function TasksPage({ onLogout }) {
         loadTasks()
     }, [])
 
+    async function handleCreateTask(event) {
+        event.preventDefault()
+        const token = localStorage.getItem('token')
+        const response = await fetch(`${API_BASE_URL}/tasks/`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Token ${token}`,
+            },
+            body: JSON.stringify({
+                title: title,
+                description: description,
+                due_date: dueDate || null,
+                priority: priority
+            }),
+        })
+        const data = await response.json()
+        if (response.ok) {
+            console.log('Task created', data)
+            loadTasks();
+            setTitle('')
+            setDescription('')
+            setDueDate('')
+            setPriotiy('medium')
+        } else {
+            console.log('Create task failed:', data)
+        }
+    }
     return (
         <div>
             <h1>TaskFlow</h1>
             <h2>My Tasks</h2>
+            <form onSubmit={handleCreateTask}>
+                <h3>Create New Task</h3>
+
+                <input type="text"
+                    placeholder="Title"
+                    value={title}
+                    onChange={(event) => setTitle(event.target.value)}
+                />
+
+                <input type="text"
+                    placeholder="Description"
+                    value={description}
+                    onChange={(event) => setDescription(event.target.value)}
+                />
+
+                <input type="date"
+                    value={dueDate}
+                    onChange={(event) => setDueDate(event.target.value)}
+                />
+
+                <select value={priority}
+                    onChange={(event) => setPriotiy(event.target.value)}
+                >
+                    <option value="low">Low</option>
+                    <option value="medium">Medium</option>
+                    <option value="high">High</option>
+                </select>
+
+                <button type='submit'>Add Task</button>
+            </form>
             {tasks.length === 0 ? (
                 <p>No tasks yet.</p>
             ) : (
